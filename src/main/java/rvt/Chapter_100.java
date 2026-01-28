e5b83dab22005171d020a2c40f8298b138c1db77
24DP3JFrol <A240132jf@rvt.lv>
2026-01-28 10:27:37 +0200

    Preparing for assignments from connecticut 100

---
+++ src/main/java/rvt/Chapter_100.java
@@ -0,0 +1,10 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Chapter_100 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input;
+    }
+}
